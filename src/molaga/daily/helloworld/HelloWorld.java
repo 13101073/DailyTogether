8cc947ea176378e3db4d519b2b08849bd237f0d5
molaga <13101073@users.noreply.github.com>
2015-12-24 17:29:37 +0800

    PracticeGather

---
+++ src/molaga/daily/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package molaga.daily.helloworld;
+
+/**
+ * @create 2015.12.24 17:20
+ * @author molaga
+ * @tips 这只是日常练习的集合的开始
+ * @tips -version_EN: This is just a start of molaga's daily practice.<br/> 
+ * <pre><b>Study/Unlimited Coding Works</b></pre>
+ * */
+public class HelloWorld {
+    
+    public static void main(String args[]){
+        System.out.println("hello, molaga~");
+        System.out.println("use 4 spaces instead of a tab input.");
+    }
+}
